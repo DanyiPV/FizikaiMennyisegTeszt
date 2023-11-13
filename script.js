0291d6359ee5173c6116315cb3fe10ab7dd26861
PDani <pinteadani88@gmail.com>
2023-11-13 19:37:36 +0100

    SideBar asszem kész

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var AlapDiv = document.getElementsByClassName('AlapDiv')[0];
 var SideBarOpen = 0;
+var TablazatokOpen = 0;
 var Timer = setInterval(Timer,1000);
 function Fooldal(){
     AlapDiv.innerHTML="";
@@ -25,6 +26,11 @@ function CloseSideBar(){
         document.getElementById('SideBarDiv').classList.remove('SideBarDivOpen');
         document.getElementById('BlackBackground').classList.remove('BlackBackgroundActive');
         document.getElementById('BlackBackground').removeAttribute('onclick',"CloseSideBar()")
+        if(TablazatokOpen == 1){
+            document.getElementById('TabazatokDiv').classList.remove('TabazatokDivOpen');
+            document.getElementById('Tablazatok').classList.remove('h2Active');
+            TablazatokOpen=1;
+        }
     }
 }
 function Timer(){
@@ -33,6 +39,17 @@ function Timer(){
         let hour = d.getHours();
         let min = d.getMinutes()
         document.getElementById('Time').innerHTML = "<p>"+(hour<10?"0"+hour:hour)+":"+(min<10?"0"+min:min)+"</p>"
+    }
+}
+function TablazatokMegjelenit(){
+    if(TablazatokOpen == 0){
+        TablazatokOpen=1;
+        document.getElementById('TabazatokDiv').classList.add('TabazatokDivOpen');
+        document.getElementById('Tablazatok').classList.add('h2Active');
+    }else if(TablazatokOpen == 1){
+        TablazatokOpen=0;
+        document.getElementById('TabazatokDiv').classList.remove('TabazatokDivOpen');
+        document.getElementById('Tablazatok').classList.remove('h2Active');
     }
 }
 Fooldal();
