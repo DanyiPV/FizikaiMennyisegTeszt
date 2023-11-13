5937d9deb478483c628772c46802be12e388f759
PDani <pinteadani88@gmail.com>
2023-11-13 19:03:37 +0100

    Sidebar mindjá kész

--- script.js
+++ script.js
@@ -8,30 +8,31 @@ function Fooldal(){
     FoTittle.innerHTML="<h2>Próbáld ki, mennyire értesz a Fizikához!</h2>";
     AlapDiv.appendChild(FoTittle);
 }
-
 function ProbaTest(){
     AlapDiv.innerHTML="";
 }
-
 function SideBar(){
     if(SideBarOpen == 0){
         SideBarOpen=1;
         document.getElementById('SideBarDiv').classList.add('SideBarDivOpen');
+        document.getElementById('BlackBackground').classList.add('BlackBackgroundActive');
+        document.getElementById('BlackBackground').setAttribute('onclick',"CloseSideBar()")
     }
 }
-
 function CloseSideBar(){
     if(SideBarOpen == 1){
         SideBarOpen=0;
         document.getElementById('SideBarDiv').classList.remove('SideBarDivOpen');
+        document.getElementById('BlackBackground').classList.remove('BlackBackgroundActive');
+        document.getElementById('BlackBackground').removeAttribute('onclick',"CloseSideBar()")
     }
 }
-
 function Timer(){
-    const d = new Date();
-    let hour = d.getHours();
-    let min = d.getMinutes()
-    document.getElementById('Time').innerHTML = "<p>"+(hour<10?"0"+hour:hour)+":"+(min<10?"0"+min:min)+"</p>"
+    if(SideBarOpen == 1){
+        const d = new Date();
+        let hour = d.getHours();
+        let min = d.getMinutes()
+        document.getElementById('Time').innerHTML = "<p>"+(hour<10?"0"+hour:hour)+":"+(min<10?"0"+min:min)+"</p>"
+    }
 }
-
 Fooldal();
