39c664fe0d397cd41beaedefca64d9bb27ecdebc
Pintea Roland Dániel <pintea.roland@ckik.loc>
2024-03-19 10:37:02 +0100

    test oldal rework

---
+++ test_script.js
@@ -0,0 +1,3 @@
+function SideBarOpen(){
+    document.getElementById("Page").classList.contains("PageOpen")?document.getElementById("Page").classList.remove("PageOpen"):document.getElementById("Page").classList.add("PageOpen");
+}
