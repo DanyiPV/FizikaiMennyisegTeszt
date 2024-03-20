d638f4e1850d605235f35cd3d36a3651fa9263ee
PDani <pinteadani88@gmail.com>
2024-03-20 23:09:16 +0100

    redesign főoldal

--- test_script.js
+++ test_script.js
@@ -1,3 +1,30 @@
 function SideBarOpen(){
-    document.getElementById("SideBarNav").classList.contains("SideBarNavOpen")?document.getElementById("SideBarNav").classList.remove("SideBarNavOpen"):document.getElementById("SideBarNav").classList.add("SideBarNavOpen");
+    document.getElementById("SideBarNav").classList.add("SideBarNavOpen");
+    document.getElementById("BlackBG").classList.add("BlackBGOn");
+    document.getElementById("BlackBG").setAttribute("onclick","SideBarClose()");
 }
+
+function SideBarClose(){
+    document.getElementById("SideBarNav").classList.remove("SideBarNavOpen");
+    document.getElementById("BlackBG").classList.remove("BlackBGOn");
+    document.getElementById("BlackBG").removeAttribute("onclick");
+}
+
+function SignInOpen(){
+    document.getElementById("SignInNav").classList.add("SignInOpen");
+    document.getElementById("SignIn").classList.add("SignInDivOpen");
+    document.getElementById("BlackBG").classList.add("BlackBGOn");
+    document.getElementById("BlackBG").setAttribute("onclick","SignInClose()");
+}
+
+function SignInClose(){
+    document.getElementById("SignInNav").classList.remove("SignInOpen");
+    document.getElementById("SignIn").classList.remove("SignInDivOpen");
+    document.getElementById("BlackBG").classList.remove("BlackBGOn");
+    document.getElementById("BlackBG").removeAttribute("onclick");
+}
+
+function RemoveAnim(){
+    document.getElementById("SignIn").classList.remove("SignInAnim");
+}
+setTimeout(RemoveAnim,800);
